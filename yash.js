0ef7054b96cfc185fad3acec19c520b4ede11c93
yaswanthmanipodili <yaswanthmanipodili4@gmail.com>
2024-08-22 10:01:22 +0530

    Add files via upload

---
+++ yash.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var contactButton = document.getElementById("contactButton");
+    contactButton.addEventListener("click", function() {
+        alert("Thank you for reaching out! We will get back to you soon.");
+    });
+});
